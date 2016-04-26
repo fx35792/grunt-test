f5247255834c96d745ae46e67fc52a17ca310957
fx35792 <fx35792@163.com>
2016-04-26 15:18:03 +0800

    add file

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,24 @@ module.exports=function(grunt){
 	grunt.initConfig({
 
 		//获取package.json的信息
-		pkg:grunt.file.readJSON('package.json')
+		pkg:grunt.file.readJSON('package.json'),
+
+		//uglify插件的配置信息
+		uglify:{
+			options:{
+				stripBanners:true,
+				banner:'/*! <%=pkg.name%>-<%=pkg.version%>.js <%= grunt.template.today("yyyy-mm-dd")%> */\n'
+			},
+			build:{
+				src:'src/test.js',
+				dest:'build/<%=pkg.name%>-<%=pkg.version%>.js.min.js'
+			}
+		},
 	});
 
+	//告诉grunt我们将开始使用插件
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	//告诉grunt当我们在终端中输入grunt时需要做些什么（注意先后顺序）
-	grunt.registerTask('default',[])
+	grunt.registerTask('default',['uglify']);
 
 }
